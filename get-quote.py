197f3464ffb6fa6b85bf9f07c12263cebdc1a18c
mohammedtouheedpatel <59679306+mohammedtouheedpatelgithubcom@users.noreply.github.com>
2021-02-28 17:28:42 +0530

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,8 +1,8 @@
 def main():
    print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
+  f = open("quotes.txt")
+  quotes = f.readlines()
   #f.close()
 
   #print(quotes)
